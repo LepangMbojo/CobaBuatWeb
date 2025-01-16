906b569d44a4b676b9a2dc2fc05488f8a710b013
M. Khalid <166613766+LepangMbojo@users.noreply.github.com>
2025-01-17 02:02:40 +0800

    Add files via upload

---
+++ belajar/test1.js
@@ -0,0 +1,9 @@
+console.log('hey cik');
+let nama = prompt('masukan nama');   
+let usia = prompt('masukan umur');
+
+// let tambah = prompt['1,2'];
+// tambah.push('3'); 
+
+// alert(tambah);
+alert(`usia kamu adalah ${usia} dan nama kamu adalah ${nama}`);
